4cd29cda720794e1f69b13319521c3dc1da2a2d4
gmay <2021128159@qq.com>
2021-12-05 16:24:52 -0800

    First commit

---
+++ testmongoose.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+const schema={
+    name:String,
+    age:Number,
+    health:String
+}
+const mydata = mongoose.model('cats', schema);
+
+// const kitty = new mydata({ name1: 'testZildjian2'});
+// kitty.save().then(() => console.log('testmeow1'));
+// const kitty1 =new mydata({name:"guoguomeimei3",age:20,health:"good"})
+// kitty1.save().then(()=>console.log("new schema writed"))
+m`ydata.find({name:"Zildjian"},(err,data)=>{console.log(data[0]._doc.name)})
